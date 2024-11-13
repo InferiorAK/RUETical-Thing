c824f77b33ce7cce0927d49024e26313ef4a3f9f
InferiorAK <114951279+InferiorAK@users.noreply.github.com>
2024-11-13 08:24:42 +0600

    Add files via upload

---
+++ CSE 1101 - Lab Work/12_Nov_2024/Excercise 1/1_name_roll.c
@@ -0,0 +1,12 @@
+// Author : InferiorAK
+// Work_Done : 12 Nov 2024
+
+#include <stdio.h>
+
+int main(){
+	printf("Sample Output:\n");
+	printf("NAME: James Bond\n");
+	printf("Roll No.: 643007\n");
+	printf("Department: Crime & Ivestigation\n");
+	return 0;
+}
---
+++ CSE 1101 - Lab Work/12_Nov_2024/Excercise 1/2_pattern.c
@@ -0,0 +1,13 @@
+// Author : InferiorAK
+// Work_Done : 12 Nov 2024
+
+#include <stdio.h>
+
+int main(){
+    printf("    *\n");
+    printf("   * *\n");
+    printf("  * * *\n");
+    printf(" * * * *\n");
+    printf("* * * * *\n");
+    return 0;
+}
